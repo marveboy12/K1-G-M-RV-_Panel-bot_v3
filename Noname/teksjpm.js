71bb20d66adc3d158bf412fc2100c1015a90a124
LOSTBOY1-TCH <derekamoah009@gmail.com>
2024-12-14 14:19:46 +0000

    Add files via upload

---
+++ Noname/teksjpm.js
@@ -0,0 +1,45 @@
+*All Transaksi Open ✅*
+
+*List Market ShyoCannn Hosting 🛒*
+* Panel Run Bot Whatshapp
+* Reseller/Admin/Pt/Owner Panel
+* Script Bot Jaga Grup Free Update 3kali
+* Script Bot Cpanel + Subdomain
+* Jasa Run Scrip
+* Jasa Fix Sc 
+* Jasa Add Fitur Di Script
+* Jasa Bug Nomer Whatsapp
+* Jasa Post
+* Partner ShyoCannn
+* Murband
+* Murunband
+* DLL TANYAKAN SAJA 
+HARGA??? PM AJA REK KLO HOKI DPET HARGA LAGI DISKON
+
+
+*List Harga Panel Pterodactyl 🚀*
+* Ram 1GB : Rp1000
+* Ram 2GB : Rp2000
+* Ram 3GB : Rp3000
+* Ram 4GB : Rp4000
+* Ram Unlimited : Rp5000
+* Reseller panel : Rp5000
+* Admin panel : Rp10000
+* PT panel : Rp15000
+*Boleh Nego Asal Pake 🧠*
+*Bergaransi & Server Private*
+
+*Minat ? Hubungi :*
+* 🪀 WhatsApp
+https://wa.me/6282176642989
+* 🚀 Telegram
+https://t.me/@ShyoDes
+* 🌀 Testimoni
+https://whatsapp.com/channel/0029VarD8iiKGGG9hiMvAK18
+
+* 🐦 Grup Bebas Share*
+https://chat.whatsapp.com/J882xNKMU3oLB2cBtBbtxf
+
+
+* ⛩️Grup NakanoMD 
+https://chat.whatsapp.com/KUctG1lRvlC7ItQzE8TLvN
